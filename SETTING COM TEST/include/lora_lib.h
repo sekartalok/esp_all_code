bb52a573211d61dd5c2ea87ee334d80bc6db33f0
crayonkisser <sekartalok@gmail.com>
2025-09-12 09:40:18 +0700

    test

---
+++ SETTING COM TEST/include/lora_lib.h
@@ -0,0 +1,12 @@
+#pragma once 
+
+#include <RadioLib.h>
+
+// LoRa Module pins
+#define NSS   7
+#define RST   8
+#define DIO1  33
+#define BUSY  34
+#define SCK   5
+#define MISO  3
+#define MOSI  6
