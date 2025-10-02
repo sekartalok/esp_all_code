bca80c166eb6bec6087c2bae6c113cf9d5900068
crayonkisser <sekartalok@gmail.com>
2025-10-02 12:06:31 +0700

    check point

--- ISP_MULTISENSOR _TEST/lib/ICM2048LIB/ICM2048DMA.cpp
+++ ISP_MULTISENSOR _TEST/lib/ICM2048LIB/ICM2048DMA.cpp
@@ -13,15 +13,16 @@ void ICM20948_DMA::sleep(bool sleep) {
 void ICM20948_DMA::switchBank(uint8_t newBank) {
     if (newBank != currentBank) {
         currentBank = newBank;
-
-        master->beginTransaction(spi_setting);
         digitalWrite(csPin, LOW);
+        master->beginTransaction(spi_setting);
+        
 
         master->transfer(static_cast<uint8_t>(ICM20948_All_Bank::ICM20948_REG_BANK_SEL) & ICM20948_WRITE_MASKING_BIT);
         master->transfer((newBank & ICM20948_BANK_MASKING_BIT ) << 4);
 
-        digitalWrite(csPin, HIGH);
+        
         master->endTransaction();
+        digitalWrite(csPin, HIGH);
 
         delayMicroseconds(10);
     }
@@ -30,20 +31,22 @@ void ICM20948_DMA::switchBank(uint8_t newBank) {
 void ICM20948_DMA::reset_ICM20948() {
     writeRegister8(0, ICM20948_PWR_MGMT_1,
                    static_cast<uint8_t>(REGISTER_BITS::ICM20948_RESET));
-    delay(100); // 100 ms, not 5 ms
+    delay(100); 
 }
 
 void ICM20948_DMA::writeRegister8(uint8_t bank, uint8_t reg, uint8_t val) {
     switchBank(bank);
 
-    master->beginTransaction(spi_setting);
     digitalWrite(csPin, LOW);
+    master->beginTransaction(spi_setting);
+    
 
     master->transfer(reg & ICM20948_WRITE_MASKING_BIT); // write mask
     master->transfer(val);
 
-    digitalWrite(csPin, HIGH);
+    
     master->endTransaction();
+    digitalWrite(csPin, HIGH);
 
     delayMicroseconds(5);
 }
@@ -53,14 +56,16 @@ uint8_t ICM20948_DMA::readRegister8(uint8_t bank, uint8_t reg) {
 
     uint8_t value = 0;
 
-    master->beginTransaction(spi_setting);
     digitalWrite(csPin, LOW);
+    master->beginTransaction(spi_setting);
+    
 
     master->transfer(reg | ICM20948_READ_MASKING_BIT); // read mask
     value = master->transfer(0x00);
 
-    digitalWrite(csPin, HIGH);
+    
     master->endTransaction();
+    digitalWrite(csPin, HIGH);
 
     delayMicroseconds(5);
     return value;
@@ -76,9 +81,9 @@ bool ICM20948_DMA::init() {
 
     spi_setting = SPISettings(7000000, MSBFIRST, SPI_MODE0);
 
-    currentBank = 0xFF; // force first switch
+    currentBank = 0xFF; 
 
-    reset_ICM20948();   // one reset only
+    reset_ICM20948();   
     delay(100);
 
     uint8_t tries = 0;
